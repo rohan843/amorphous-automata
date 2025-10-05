f5839570f4c64d7077b3775e09f387da752a22a6
rohan843 <rohan.sharma2934@gmail.com>
2025-10-05 15:34:44 +0530

    Setup resizable logo component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,10 @@
 import logo from "../assets/logo.png";
+import classNames from "classnames";
 
 function Logo({ classname }) {
   return (
-    <div className={classname}>
-      <img src={logo} alt="Logo" />
+    <div className={classNames("logo-container", classname)}>
+      <img src={logo} alt="Logo" className="logo-image" />
     </div>
   );
 }
